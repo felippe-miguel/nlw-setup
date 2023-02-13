7b4c89d2d9a0506ef3545a3f659ad018b76d13a5
Felippe Miguel <felippe.miguel.silva@gmail.com>
2023-02-13 08:28:26 -0300

    new routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,15 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify();
-const prisma = new PrismaClient();
+const port = 3333;
 
 app.register(cors);
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({
-  port: 3333
+  port: port
 }).then(() => {
-  console.log('HTTP Server running!');
+  console.log(`HTTP Server running on port :${port}`);
 });
